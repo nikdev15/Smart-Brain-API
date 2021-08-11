902fae0cc32b74b9a1184afe0eb82c610b1c7711
unknown <nikhilisaac1519@gmail.com>
2021-08-11 21:01:48 +0530

    adding port

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res,)})
 
-app.listen(3001, ()=> {
-	console.log('app is running');
+app.listen(process.env.PORT || 3001, ()=> {
+	console.log(`app is running ${process.env.PORT}`);
 })
